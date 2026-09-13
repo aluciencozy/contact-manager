b6fbc9e87ccf4c856272709eb2540b3a9e5eb177
James O'Donnell <mageoflight413@gmail.com>
2026-09-13 12:29:32 -0400

    Refactor registration logic and improve security
    reworked some of how registration Api works, attempted to mirror login.

--- api/register.php
+++ api/register.php
@@ -1,72 +1,103 @@
 <?php
 // handle POST /register
 
-//replace with proper table name later
-$tableName='temp';
+session_start();
 
-$username=$_POST['username'];
-$email=$_POST['email'];
-$phone=$_POST['phone'];
-$password=$_POST['password'];
+//placeholder tablename need to check with database
+$tableName='Users';
 
+//inform frontend of result type
+//header('Content-Type: application/json; charset=utf-8');
+
+//get variables from database
+require __DIR__ . "/config/database.php";
+
+
+//decodes input allowing to get variables
+$user_data = json_decode(file_get_contents("php://input"), true);
+
+$username=$user_data["username"] ?? "";
+$email=$user_data["email"] ?? "";
+$phone=$user_data["phone"] ?? "";
+$password=$user_data["password"] ?? "";
 
 //validates that the input data is not Null and meets requirements
-if($username==Null){
+if($username==""){
     exit('Invalid username');
 }
 
 //uses built in Email check
-if($email==Null || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+if($email=="" || !filter_var($email, FILTER_VALIDATE_EMAIL)){
     exit('Invalid email');
 }
 
 //ensures phone number is 10 char long and only 0-9
-if($phone==Null || strlen($phone)!=10 || !preg_match('/^[0-9]+$/', $phone)){
+if($phone=="" || strlen($phone)!=10 || !preg_match('/^[0-9]+$/', $phone)){
     exit('Invalid phone number');
 }
 
-if($password==Null){
+if($password==""){
     exit('Invalid password');
 }
 
 
 //connects to database
-$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
-if($mysqli->connect_error){
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+if($conn->connect_error){
     exit('connection failed');
 }
 
 
-//checks all values within a column for a specifc value
-$sqlUsername = "SELECT username FROM $tableName WHERE username='$username'";
-$sqlEmail = "SELECT email FROM $tableName WHERE email='$email'";
-$sqlPassword = "SELECT phone FROM $tableName WHERE phone='$phone'";
-
+//checks the values within the database for if username exists already
+$stmt = $conn->prepare("SELECT username FROM $tableName WHERE username=?"); //prepared statement to protect against SQL injection
+$stmt->bind_param("s", $username); 
+$stmt->execute(); 
 
 //result returns the number of number of rows with the value found by the prior check
-$result=$mysqli->query($sqlUsername);
+$result=$stmt->get_result();
 if($result->num_rows > 0){
     exit('An account with this username already exists');
 }
+$stmt->close();
 
-$result=$mysqli->query($sqlEmail);
+//this code currently prevents an account from being made if one with the corresponding email or phone already exists
+$stmt = $conn->prepare("SELECT email FROM $tableName WHERE email=?");
+$stmt->bind_param("s", $email); 
+$stmt->execute(); 
+$result=$stmt->get_result();
 if($result->num_rows > 0){
     exit('An account with this email already exists');
 }
+$stmt->close();
 
-$result=$mysqli->query($sqlPassword);
+$stmt = $conn->prepare("SELECT phone FROM $tableName WHERE phone=?");
+$stmt->bind_param("s", $phone); 
+$stmt->execute(); 
+$result=$stmt->get_result();
 if($result->num_rows > 0){
     exit('An account with this phone number already exists');
 }
+$stmt->close();
 
-
-//encrypts the password for safe storage use validate to retrieve it later
+//encrypts the password for safe storage use validate to compare later
 $hashPassword=password_hash($password, PASSWORD_DEFAULT);
 
+//generates a unique id. possibly implement after checking how database is handling ids to prevent conflict.
+/*
+use uniqueid in a while loop. loops while id exists in table
+id=uniqueid()
+while unique in table{
+    id=uniqueid()
+}
+prevents multiple same ids
+*/
+
 //inserts the values into the database
-$sql = "INSERT INTO $tableName (username, email, phone, password)
-VALUES ('$username', '$email', '$phone', '$hashPassword')";
-$mysqli->query($sql);
+$stmt =  $conn->prepare("INSERT INTO $tableName (username, email, phone, password_hash)
+VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", '$username', '$email', '$phone', '$hashPassword'); 
+$stmt->execute(); 
+$stmt->close();
 
 //closes connection
-$mysqli->close();
+$conn->close();
