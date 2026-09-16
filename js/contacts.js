55908e2a31c3ba427cb31da23ba81e771eb94499
NickTheBrick04 <nickwesterfeld@gmail.com>
2026-09-16 19:51:32 -0400

    contacts.js -Adding POST for creating new contact -Working on GET to populate all contact cards
    contacts.html:
    -included contact result message after POST call
    -created contact card container

--- js/contacts.js
+++ js/contacts.js
@@ -2,19 +2,63 @@ const addButton = document.getElementById("addContactButton");
 const dialog = document.getElementById("contactDialog");
 const cancelButton = document.getElementById("cancelContactButton");
 const contactForm = document.getElementById("contactForm");
+const contactsContainer = document.getElementById("contactsContainer");
+
 
 addButton.addEventListener("click", () => { dialog.showModal(); });
 
 cancelButton.addEventListener("click", () => { dialog.close(); });
 
-contactForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
+const contactResultMessage = document.getElementById("contactResultMessage");
 
-    //fill in functionality
+contactForm.addEventListener("submit", async (event) => {
+    //making new contact and POSTing to DB
+  event.preventDefault();
+    const newContactData = new FormData(contactForm);
+    const firstName = newContactData.get("first_name");
+    const lastName = newContactData.get("last_name");
+    const email = newContactData.get("email");
+    const phoneNumber = newContactData.get("phone_number");
+
+    const response = await fetch("/api/contacts/create.php",{
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({firstName, lastName,
+            email,  phoneNumber}),
+    }
+    );
+
+    const result = await response.json();
+
+    if (response.ok && result.success){
+        getContactInfo();
+        contactResultMessage.textContent = "New contact created!"
+        contactResultMessage.style.color = "green"
+    }
+    else{
+        contactResultMessage.textContent = "failed to make a new contact" + result.message;
+        contactResultMessage.style.color = "red"
+    }
+
   dialog.close();
 });
 
-contactForm
+// once the new contact is created we will reload all contacts using GET
+const getContactInfo = async () =>{
+
+    const response = await fetch("/api/contacts/get.php",{
+        method: "GET", 
+    })
+
+    const result = await response.json();
+
+    contactsContainer.textContent = "";
+
+
+
+}
+
+
 
 // all contact CRUD logic
 
