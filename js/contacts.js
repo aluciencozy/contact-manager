9f100574835ae517a79944dae8b3ac0ce2963100
NickTheBrick04 <nickwesterfeld@gmail.com>
2026-09-18 18:44:54 -0400

    Removing conflicting logout function

--- js/contacts.js
+++ js/contacts.js
@@ -59,24 +59,3 @@ const getContactInfo = async () =>{
 }
 
 
-
-// all contact CRUD logic
-
-const logoutButton = document.getElementById("logoutButton");
-
-if (logoutButton) {
-    logoutButton.addEventListener("click", async () => {
-        try {
-            // Communicate with the backend to log out the user
-
-            // if logout is successful, redirect to login page
-
-            if (1) { // Replace with actual logout check
-                window.location.href = "index.html";
-            }
-
-        } catch (error) {
-            console.error("Logout failed:", error);
-        }
-    });
-}
